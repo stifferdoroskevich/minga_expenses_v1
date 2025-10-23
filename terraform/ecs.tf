dd9538c7fecaf65489b1bf3de34d7cb03a6c7dcf
Stiffer Doroskevich <stiffer17@gmail.com>
2025-10-23 21:33:50 +0200

    fix: aumented master lists on dropdown

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -17,8 +17,8 @@ resource "aws_ecs_task_definition" "django_app" {
   family                   = "${var.project_name}-django"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 512
-  memory                   = 1024
+  cpu                      = 256
+  memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   task_role_arn           = aws_iam_role.ecs_task_role.arn
 
